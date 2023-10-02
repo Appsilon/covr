d2140ee2ecd354d91d62100851c3e2061f29016c
Rodrigo Basa <rodrigo@appsilon.com>
2023-10-02 21:28:40 +0800

    how many does file_coverage return?

--- tests/testthat/test-box.R
+++ tests/testthat/test-box.R
@@ -9,6 +9,9 @@ test_that("box module coverage is reported", {
         source_files = "app/app.R",
         test_files = list.files("tests/testthat", full.names = TRUE)))
 print(list.files("tests/testthat", full.names = TRUE))
+print(str(file_coverage(
+  source_files = "app/app.R",
+  test_files = list.files("tests/testthat", full.names = TRUE))))
     expect_equal(cov$value, c(5, 2, 3, 3, 1, 1))
     expect_equal(cov$first_line, c(5, 6, 8, 13, 5, 8))
     expect_equal(cov$last_line, c(5, 6, 8, 13, 5, 8))
