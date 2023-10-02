96190c0df0970a5dead458b580ee2593d04e0610
Rodrigo Basa <rodrigo@appsilon.com>
2023-10-02 20:14:41 +0800

    remove print statements

--- tests/testthat/test-box_attached_modules_functions.R
+++ tests/testthat/test-box_attached_modules_functions.R
@@ -9,7 +9,6 @@ test_that("box attached module coverage is reported", {
       source_files = "app/app.R",
       test_files = list.files("tests/testthat", full.names = TRUE)))
 
-    print(cov$value)
     expect_equal(cov$value, c(20, 8, 12, 3, 0, 1, 1))
     expect_equal(cov$first_line, c(5, 6, 8, 14, 18, 5, 8))
     expect_equal(cov$last_line, c(5, 6, 8, 14, 18, 5, 8))
