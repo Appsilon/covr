cb62173e78b7aa5b3972e5e86c26aaf9a0df75b3
Rodrigo Basa <rodrigo@appsilon.com>
2023-10-02 18:46:09 +0800

    will git show me results of print?

--- tests/testthat/test-box.R
+++ tests/testthat/test-box.R
@@ -9,6 +9,7 @@ test_that("box module coverage is reported", {
         source_files = "app/app.R",
         test_files = list.files("tests/testthat", full.names = TRUE)))
 
+    print(cov$value)
     expect_equal(cov$value, c(5, 2, 3, 3, 1, 1))
     expect_equal(cov$first_line, c(5, 6, 8, 13, 5, 8))
     expect_equal(cov$last_line, c(5, 6, 8, 13, 5, 8))
