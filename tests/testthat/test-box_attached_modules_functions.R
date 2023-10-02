fb79408c0a5b076f3ea7c0123a55e81a72c1b1ae
Rad Basa <radbasa@ubuntu-20.04>
2023-10-02 23:52:33 +0800

    update box test cases

--- tests/testthat/test-box_attached_modules_functions.R
+++ tests/testthat/test-box_attached_modules_functions.R
@@ -9,12 +9,11 @@ test_that("box attached module coverage is reported", {
       source_files = "app/app.R",
       test_files = list.files("tests/testthat", full.names = TRUE)))
 
-    expect_equal(cov$value, c(20, 8, 12, 3, 0, 1, 1))
-    expect_equal(cov$first_line, c(5, 6, 8, 14, 18, 5, 8))
-    expect_equal(cov$last_line, c(5, 6, 8, 14, 18, 5, 8))
+    expect_equal(cov$value, c(20, 8, 12, 3, 0))
+    expect_equal(cov$first_line, c(5, 6, 8, 14, 18))
+    expect_equal(cov$last_line, c(5, 6, 8, 14, 18))
     expect_true("a" %in% cov$functions)
     expect_true("private_function" %in% cov$functions)
-    expect_true("show" %in% cov$functions)
   })
 
 })
